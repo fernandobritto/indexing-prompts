06fb290ba11b42924b6d1b2441dddd3883ce737d
Fernando Britto <me@fernandobritto.com.br>
2017-07-11 01:30:45 +0000

    HTTP fetch client

--- AuRunContacts/src/home/index.js
+++ AuRunContacts/src/home/index.js
@@ -1,3 +1,14 @@
+import {HttpClient, json} from 'aurelia-fetch-client';
+
+let httpClient = new HttpClient();
+
+
 export class Home{
+    constructor(){
+        this.button = 'Click here to get server data';
+    }
     
+    getFromServer(){
+        console.log('HERE');
+    }
 };
