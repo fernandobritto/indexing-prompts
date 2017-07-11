9387b1429b727af1e3f8dac8fb2ada6843bdab9c
Fernando Britto <me@fernandobritto.com.br>
2017-07-11 01:17:13 +0000

    Basic Routing

--- AuRunContacts/src/app.js
+++ AuRunContacts/src/app.js
@@ -1,9 +1,30 @@
 export class App {
   constructor() {
-    this.message = 'Hello World!';
-    this.repeater = [
-      1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-      ];
+    // this.message = 'Hello World!';
+    // this.repeater = [
+    //   1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    //   ];
+  }
+  
+  configureRouter(config, router){
+    this.router = router;
+    
+    config.title = 'Aurelia - Info Enter';
+    config.map([
+      {
+        route: [''],
+        name: 'home',
+        moduleId: 'home/index',
+        title: 'Info Enter',
+        redirect: 'main'
+      },
+      {
+        route: ['main'],
+        name: 'main',
+        moduleId: 'home/index',
+        title: 'Info Enter'
+      }  
+    ])
   }
   
   alert(){
