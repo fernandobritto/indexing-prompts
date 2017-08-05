71d158f50e2c2a2c2929c213acf09978e4b35b2b
Fernando Britto <me@fernandobritto.com.br>
2017-08-05 02:43:56 +0000

    EI3305

--- AuRunContacts/src/home/index.js
+++ AuRunContacts/src/home/index.js
@@ -5,4 +5,16 @@ export class Home{
         this.heading = 'List of contacts';
         this.contacts = new Contact().getData();
     }
+    
+    deleteContact(name){
+        this.reg = this.contacts.filter((el) => {
+            return el.name == name;
+        });
+        
+        let index = this.contact.indexOf(this.reg[0]);
+        
+        if(index !== -1){
+            this.contacts.splice(index, 1);
+        }
+    }
 };
