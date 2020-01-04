fe0b96f0bac207ae7a9b49fc478bcb0c1eb5c6f4
Fernando Britto <me@fernandobritto.com.br>
2020-01-04 07:55:45 -0200

    Routes and Controller
    Created the endpoints and the controller class

---
+++ API_Lumen/app/Http/Controllers/CarsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function getAll()
+    {
+        return "getAll";
+    }
+
+    public function get($id)
+    {
+        //
+
+    }
+
+    public function store(Request $request)
+    {
+        //
+
+    }
+
+    public function update($id, Request $request)
+    {
+
+
+    }
+
+    public function delete($id)
+    {
+        //
+
+    }
+}
--- API_Lumen/routes/web.php
+++ API_Lumen/routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$route->get("/api/cars", "CarsController@getAll");
+
+$router->group(["prefix" => "/api/car"], function() use ($router){
+  $router->get("/{id}", "CarsController@get");
+  $router->post("/", "CarsController@store");
+  $router->put("/{id}", "CarsController@update");
+  $router->delete("/{id}", "CarsController@destroy");
 });
